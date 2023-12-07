6a830a40821a26eb25cd0633e2c97777b057ada0
carlo8pastor <120664886+carlo8pastor@users.noreply.github.com>
2023-12-07 02:04:44 +0100

    Delete minichallenge3 copy directory

--- minichallenge3 copy/android/app/src/main/kotlin/com/example/test1000/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.test1000
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- minichallenge3 copy/lib/screens/screens.dart
+++
@@ -1,4 +0,0 @@
-export '/screens/popular_screen.dart';
-export '/screens/movie_detail_screen.dart';
-export '/screens/watchlist_screen.dart';
-export '/screens/search_screen.dart';
